af742302c736ed787b6163f3581aa411bc9a0185
Misael <misael.alisson14@gmail.com>
2024-08-09 22:00:23 -0300

    set project images, title, font and colors, and delete next boilerplate page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+        mono: ['var(--font-poppins)'],
+      },
+      colors: {
+        "title": "var(--title)",
+        "lightgreen": "var(--lightgreen)",
+        "orange": "var(--orange)",
+        "blue": "var(--blue)",
+        "lightgrey": "var(--lightgrey)",
       },
     },
   },
