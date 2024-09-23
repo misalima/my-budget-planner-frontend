b9d6b5d183c6988e6c80069c10f2bdbb3e0bb298
Misael Lima <misael.alisson14@gmail.com>
2024-09-22 21:44:23 -0300

    set up and finish about page

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const AboutPage: React.FC = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>Welcome to My Budget Planner. This application helps you manage your finances effectively.</p>
+  return (
+    <main className="flex justify-center gap-10 mt-4 px-20">
+      <div className="flex flex-col gap-8 px-8 min-w-[400px]">
+        <h1 className="max-w-[600px] text-4xl leading-none font-medium text-blue">
+          About Us
+        </h1>
+        <div className="text-lg text-blue leading-6 text-justify max-w-[510px]">
+          <p className="mb-4">
+            My Budget Planner is an application designed to help you take
+            control of your finances and achieve your financial goals. Our
+            mission is to provide you with a powerful yet easy-to-use tool that
+            simplifies the management of your expenses, budgets, and credit
+            cards, allowing you to make informed decisions and stay on top of
+            your financial health.
+          </p>
+          <p>
+            We understand that managing money can be challenging, that&apos;s
+            why we&apos;ve created an intuitive application that streamlines the
+            process of tracking your spending, setting budgets, and monitoring
+            your credit card balances. Whether you&apos;re saving for a big
+            purchase, trying to reduce debt, or simply want to understand where
+            your money goes, My Budget Planner is here to support you every step
+            of the way.
+          </p>
         </div>
-    );
+        <div className="flex justify-left gap-6">
+          <Link href={"/login"}>
+            <button className="transition-all text-lg font-medium text-white bg-blue px-8 py-2 rounded-lg hover:bg-darkblue">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <Image
+          className="max-h-[600px]"
+          alt="about-art"
+          src={"/images/about-art.png"}
+          width={750}
+          height={360}
+        />
+      </div>
+    </main>
+  );
 };
 
 export default AboutPage;
--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 
 const RegisterView: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Handle registration logic here
-    console.log("Registering:", { username, password });
-  };
-
+ 
   return (
     <div className="text-center">
       Contact
